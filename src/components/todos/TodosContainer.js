8c0ebca12f47d0f6026696fcb6e1adb2676fcc34
prahal <vprahaladhan@gmail.com>
2021-06-27 12:17:15 +1000

    commit tree 8c5a5469bd8e787145ae7f641bbf6e909bbf5e9f

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,19 @@
+// ./src/components/todos/TodosContainer.js
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Todo from './Todo';
+
+class TodosContainer extends Component {
+
+  render() {
+    return(
+      <ul>
+        {this.props.todos.map((todo, id) => <Todo key={id} text={todo.text} />)}
+      </ul>
+    );
+  }
+};
+
+export default connect(state => { todos: state.todos })(TodosContainer);
