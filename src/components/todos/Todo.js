ebc1e40d8a67592e01041c526ef6d9a6c11b11ae
prahal <vprahaladhan@gmail.com>
2021-06-27 12:30:11 +1000

    Done.

---
+++ src/components/todos/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = ({ text }) => (
+  <div>
+    <li>{text}</li>
+  </div>
+);
+
+export default Todo;
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,12 +1,9 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
+export default function manageTodo(state = { todos: [] }, action) {
   switch (action.type) {
     case 'ADD_TODO':
-
       return { todos: state.todos.concat(action.payload.text) };
 
     default:
       return state;
   }
-}
+};
